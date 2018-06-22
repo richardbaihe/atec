0f726d791a5e003411f234ed96c870f48813ef52
Marc Wang <474733787@qq.com>
2018-06-23 01:02:53 +0800

    cv

--- model.py
+++ model.py
@@ -13,21 +13,21 @@ class XGB():
             #        params,
             # eta=0.1,
             learning_rate=0.01,
-            n_estimators=800,
+            n_estimators=2000,
             max_depth=5,
-            min_child_weight=1,
-            gamma=0.4,
+            min_child_weight=2,
+            gamma=0.2,
             silent=1,
-            subsample=0.8,
-            colsample_bytree=0.8,
+            subsample=0.9,
+            colsample_bytree=0.7,
             objective='binary:logistic',
-            scale_pos_weight=5.48,
-            max_delta_step=0.5,
+            scale_pos_weight=3.99,
+            max_delta_step=0.65,
             seed=27
         )
         # find the suitable amounts of estimators
         xgb_model.fit(train_data, train_label, eval_set=[(val_data, val_label)], eval_metric='auc',
-                      early_stopping_rounds=10)
+                      early_stopping_rounds=200)
         joblib.dump(xgb_model, self.model_name, protocol=2)
 
     def predict(self,test_data):
